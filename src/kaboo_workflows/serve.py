a87ce9d3df2f63285578690c1e589ec1bedb86aa
Paul Gege <pgege@Pauls-MacBook-Pro-2.local>
2026-07-06 08:23:19 -0400

    fix: disable OTEL instrumentation to prevent context errors

--- src/kaboo_workflows/serve.py
+++ src/kaboo_workflows/serve.py
@@ -11,6 +11,10 @@ Usage::
 from __future__ import annotations
 
 import argparse
+import os
+
+os.environ.setdefault("OTEL_SDK_DISABLED", "true")
+os.environ.setdefault("OTEL_PYTHON_DISABLED_INSTRUMENTATIONS", "all")
 
 import uvicorn
 
